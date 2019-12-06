ea2faf0c352a218d3dd3d2626cf48202be44a938
thang21197 <48194900+thang21197@users.noreply.github.com>
2019-12-07 00:47:03 +0700

    1

--- app/Helper/function.php
+++
@@ -1,8 +0,0 @@
-<?php
- function ShowError($errors,$name){
-   if ($errors->has($name)) {
-   	return '<div class="alert alert-warning">
-    <strong>'.$errors->first($name).'</strong> 
-     </div>';
-   }
- }
