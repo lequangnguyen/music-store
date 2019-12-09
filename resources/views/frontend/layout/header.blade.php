020321ed52d3899fa88eb23d89d3d1db124eaf49
thang21197 <48194900+thang21197@users.noreply.github.com>
2019-12-09 19:43:14 +0700

    Revert "Revert "Revert "layout"""
    This reverts commit 69c8e3fa4cacd8eb6ed1af079601f34b92090b84.

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -88,8 +88,9 @@
               <!-- logo  -->
               <div class="aa-logo">
                 <!-- Text based logo -->
-                <a href="/">
-                  <img src="/img/logo.png">
+                <a href="index.html">
+                  <span class="fa fa-shopping-cart"></span>
+                  <p>daily<strong>Shop</strong> <span>Your Shopping Partner</span></p>
                 </a>
                 <!-- img based logo -->
                 <!-- <a href="index.html"><img src="img/logo.jpg" alt="logo img"></a> -->
