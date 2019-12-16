a901b7d5452db1f8282fab9ecddf6317689d70a7
thang21197 <48194900+thang21197@users.noreply.github.com>
2019-12-16 18:36:45 +0700

    1

--- app/Http/Controllers/Frontend/CheckoutController.php
+++ app/Http/Controllers/Frontend/CheckoutController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\{Orders,OrderDetail};
 use Cart;
+use App\User;
 
 class CheckoutController extends Controller
 {
@@ -21,12 +22,19 @@ class CheckoutController extends Controller
     	}
     }
     function PostCheckout(Request $r){
+        // dd((int)Cart::total()/10);
     	$data['cart']=Cart::content();
         $data['total']=Cart::total(0,"",".");
+        $user=User::find(Auth::id());
 		$order=new Orders;
 		$order->user_id=Auth::id();
 		$order->status=0;
+        $user->point=$user->point+(int)Cart::total()/10;
+        if (Cart::count()<=0) {
+            return redirect('/cart');
+        }else{
 		$order->save();
+        $user->save();
         foreach(Cart::content() as $row){
             $order_detail=new OrderDetail;
             $order_detail->product_id=$row->id;
@@ -37,5 +45,6 @@ class CheckoutController extends Controller
         }
         Cart::destroy();
         return view('frontend.cart.checkout',$data);
+        }
     }
 }
